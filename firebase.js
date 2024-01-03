9e9312a0110cb18fcb0b52f4229f413ae3776801
Benard Haxhijaj <benardhaxhijaj@gmail.com>
2024-01-03 17:32:49 +0100

    Adding firebase

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLCMUKb0nCbsQwDDLOOuryZtPn_U2yHi0",
+  authDomain: "react-notes-30233.firebaseapp.com",
+  projectId: "react-notes-30233",
+  storageBucket: "react-notes-30233.appspot.com",
+  messagingSenderId: "961258099276",
+  appId: "1:961258099276:web:726f71ea72f909f4785ae9",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const notesCollections = collection(db, "notes");
